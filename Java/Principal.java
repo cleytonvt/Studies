0addbd1d26ee10cc1af5ad637a13caf63995ee5c
Cleyton Victor <cleytonvfsb@gmail.com>
2021-03-18 20:44:14 -0300

    inclusao do override

--- Java/Principal.java
+++ Java/Principal.java
@@ -1,4 +1,3 @@
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,10 +16,8 @@ private int[] posX = {5,75,145,215};
 private int[] posY = {5,55,125,195,265};   
 private int larg = 65;
 private int alt = 65;
-private int memo = 0;
-private int read = 0;
-private int i, j = 0, k= 0, l = 0, m = 0, n = 1, o = 4;
-
+private int i, j = 0, k= 0, l = 0, m = 0, n = 1, o = 4, w = 0;
+int read = 0;
 
 public static void main(String[] args) {
 	
@@ -41,8 +38,8 @@ private void go() {
 		
 	visor = new JTextField();								//instanciando JTextField 
 	visor.setEnabled(false);								//desabilita a alteracao do campo texto
-    visor.setBounds(posX[0], posY[0], larg+210, alt-20); 	//posicionamento (distancia da esquerda,distancia do topo, largura, altura)
-    frame.add(visor);
+	visor.setBounds(posX[0], posY[0], larg+210, alt-20); 	//posicionamento (distancia da esquerda,distancia do topo, largura, altura)
+	frame.add(visor);
 	
 
 	/*
@@ -50,7 +47,7 @@ private void go() {
 	utilizado laço de repetição para os números e os botões de operação
 	*/
 	
-		for ( i = 0; i <= 16; i++ )
+		for ( i = 0; i < 16; i++ )
 		{	
 			if (i >= 1 && i <= 3){
 				botoes[i] = new JButton(String.valueOf(i));
@@ -95,8 +92,22 @@ private void go() {
 				o++;
 			}
 		}
-
-	
-	
+		
+		for(w = 1; w < 10 ; w++){
+			
+				botoes[w].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						/*read *= 10;*/
+						read = w;
+						visor.setText(visor.getText() + String.valueOf(w));
+					}
+				});
+		}
 	}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 }
