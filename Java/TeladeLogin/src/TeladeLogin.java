1b83aa1d0e9ac934f00568ce9a2436bee9283dbf
Cleyton Victor <cleytonvfsb@gmail.com>
2021-04-11 18:26:19 -0300

    complete

--- Java/TeladeLogin/src/TeladeLogin.java
+++ Java/TeladeLogin/src/TeladeLogin.java
@@ -1,4 +1,4 @@
-package TeladeLogin.src;
+import java.awt.Button;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,17 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class TeladeLogin extends Application{
-    public void start(Stage stage) throws Exception{
-        Parent parent = FXMLLoader.load(getClass().getResource("tela.fxml"));
-        Scene scene = new Scene(parent);
-        stage.setScene(scene);
-        stage.setTitle("Tela de Login");
-        stage.show();
-    }
 
-    public static void main(String[] args){
-        launch(args);
-    }
+	public void start (Stage stage) throws Exception{
+		Parent parent = FXMLLoader.load(getClass().getResource("tela.fxml"));
+		Scene scene = new Scene(parent);
+		stage.setScene(scene);
+		stage.setTitle("Tela de Login");
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
 }
